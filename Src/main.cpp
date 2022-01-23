8f6c844f60fb04562f50c01609a3ec53e1c44862
FountainOfGib <dfo4242@gmail.com>
2022-01-23 12:59:00 +0000

    added collecting fruit & game over mechanics

--- Src/main.cpp
+++ Src/main.cpp
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
+#include <conio.h>
+#include <time.h>
 
 using namespace std;
 bool gameOver;
@@ -27,44 +29,108 @@ void Setup()
 void Draw()
 {
     system("cls");
-    for (int i = 0; i < width+2; i++)
+    for (int i = 0; i < width+3; i++)
         cout << "#";
     cout << endl;
 
-    for (int i = 0; i < height; i++)
+    for (int i = 0; i < height+1; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width+2; j++)
         {
             if (j==0)
             {
                 cout << "#";
             }
+            if (i==y && j==x)
+            {
+                cout << "O";
+            }else if (i==fruitY && j==fruitX)
+            {
+                cout << "@";
+            }else
+            {
+                cout << " ";
+            }
 
-            cout << " ";
-
-            if (j == width-1)
+            if (j == width)
             {
                 cout << "#";
             }
         }
         cout << endl;
     }
-    for (int i = 0; i < width +2; i++)
+    for (int i = 0; i < width +3; i++)
         cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+    cout << "x: " << x << endl;
+    cout << "y: " << y << endl;
 }
 
 void Input()
 {
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+            case 'w':
+                dir = UP;
+                break;
 
+            case 'a':
+                dir = LEFT;
+                break;
+
+            case 's':
+                dir = DOWN;
+                break;
+
+            case 'd':
+                dir = RIGHT;
+                break;
+
+            case 27:
+                gameOver = true;
+                break;
+        }
+    }
 }
 
 void logic()
 {
-
+    switch (dir)
+    {
+        case LEFT:
+            x--;
+            break;
+        
+        case RIGHT:
+            x++;
+            break;
+        
+        case UP:
+            y--;
+            break;
+        
+        case DOWN:
+            y++;
+            break;
+    }
+    if (x < 0 || y < 0 || x > width || y > height)
+    {
+        gameOver = true;
+    }
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+    }
 }
 
 int main()
 {
+    srand(time(nullptr));
     Setup();
     while(!gameOver)
     {
@@ -73,5 +139,8 @@ int main()
         logic();
         this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    Draw();
+    cout << "GAME OVER!" << endl;
+    system("pause");
     return 0;
 }
